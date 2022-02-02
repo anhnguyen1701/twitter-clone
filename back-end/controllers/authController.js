4684620fc37b97366f9d092dfb559d59ea709784
anh nguyen <nguyenlamanh1701@gmail.com>
2022-02-03 00:46:32 +0700

    ma hoa token, tao token key

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,19 +1,40 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
+    const token = jwt.sign({ userId: user._id }, process.env.APP_SECRET);
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
-        user
-      }
-    })
+        token,
+        userName: user.name,
+      },
+    });
   } catch (error) {
     res.json(error);
   }
 };
 
-exports.login = (req, res, next) => {
-  res.json("User login");
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      //err: email is not correct
+    }
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      const token = jwt.sign({ userId: user._id }, process.env.APP_SECRET);
+      res.status(200).json({
+        status: "success",
+        data: {
+          token,
+          userName: user.name,
+        },
+      });
+    } else {
+      //err: password is not correct
+    }
+  } catch (error) {}
 };
