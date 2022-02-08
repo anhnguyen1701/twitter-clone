bdcc2cb38c88a43e88a725f653455bd88bff1194
nguyenlamanh1701@gmail.com <nguyenlamanh1701@gmail.com>
2022-02-08 22:24:24 +0700

    tao AppContext

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,20 +6,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './components/NotFound';
 import { useReducer } from 'react';
 import AppReducer from './reducers/AppReducer';
+import AppContext from './components/AppContext';
+
 function App() {
   const initialState = { user: null, posts: [] };
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
   return (
     <Router>
-      <div className="container">
-        <Header></Header>
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/" element={<Main />} />
-          <Route exact path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <div className="container">
+          <Header></Header>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/" element={<Main />} />
+            <Route exact path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </AppContext.Provider>
     </Router>
   );
 }
