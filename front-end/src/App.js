91500ee92c6b3a30acc8bf56a90ea39b17035241
nguyenlamanh1701@gmail.com <nguyenlamanh1701@gmail.com>
2022-02-08 22:19:17 +0700

    tao initialState va thiet lap AppReducer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,8 +4,11 @@ import Register from './components/Register';
 import Main from './components/Main';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './components/NotFound';
-
+import { useReducer } from 'react';
+import AppReducer from './reducers/AppReducer';
 function App() {
+  const initialState = { user: null, posts: [] };
+  const [state, dispatch] = useReducer(AppReducer, initialState);
   return (
     <Router>
       <div className="container">
