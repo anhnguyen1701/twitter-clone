6ee7b1d8461f16a07725742f7ed292dc40b4e731
nguyenlamanh1701@gmail.com <nguyenlamanh1701@gmail.com>
2022-02-09 21:50:45 +0700

    cap nhap nav(menu) theo trang thai user

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from './AppContext';
 import '../css/Header.css';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const { state, dispatch } = useContext(AppContext);
+  const { user } = state;
+  const signOut = () => {
+    localStorage.removeItem('token');
+    dispatch({ type: 'CURRENT_USER', payload: null });
+  };
+
   return (
     <header className="header">
       <h1 className="logo">
-        <Link to='/'>twitter</Link>
+        <Link to="/">twitter</Link>
       </h1>
       <nav>
         <ul className="main-nav">
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-          <li>
-            <Link to='/register'>Register</Link>
-          </li>
-          <li>
-            <span href="#" className="user-name">
-              Hello, Anh Nguyen
-            </span>
-          </li>
-          <li>
-            <a href="#">Sign out</a>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <span className="user-name">Hello, {user.userName}</span>
+              </li>
+              <li onClick={() => signOut()}>
+                <a>Sign Out</a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
