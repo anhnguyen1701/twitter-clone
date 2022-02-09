0758b8d81384e8db608fccc1f4202e96e8b6f466
nguyenlamanh1701@gmail.com <nguyenlamanh1701@gmail.com>
2022-02-09 17:07:28 +0700

    ghép be Login()

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import '../css/Auth.css';
+import axios from 'axios';
+import AppContext from './AppContext';
+import { useNavigate } from 'react-router-dom';
 export default function Login() {
+  const { dispatch } = useContext(AppContext);
+  const [userInput, setUserInput] = useState({ email: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const onChangeHandle = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
+  const onSubmitHandle = async (e) => {
+    try {
+      e.preventDefault();
+      const option = {
+        method: 'post',
+        url: '/api/v1/auth/login',
+        data: userInput
+      };
+      const response = await axios(option);
+      const { token, userName } = response.data.data;
+      localStorage.setItem('token', token);
+      dispatch({ type: 'CURRENT_USER', payload: { userName } });
+      navigate('/');
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
+
   return (
     <section className="auth-container">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={onSubmitHandle}>
         <h2>Enter Your Account</h2>
-        <div className="error-message">Error: Your password is not correct</div>
-        <input type="email" name="email" id="" required placeholder="Email" />
+        {errorMessage && (
+          <div className="error-message">Error: {errorMessage}</div>
+        )}
+        <input
+          type="email"
+          name="email"
+          id=""
+          required
+          placeholder="Email"
+          value={userInput.email}
+          onChange={onChangeHandle}
+        />
         <input
           type="password"
           name="password"
           id=""
           required
           placeholder="Password"
+          value={userInput.password}
+          onChange={onChangeHandle}
         />
         <button className="btn" type="submit">
           Login
