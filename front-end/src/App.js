02536439a2bdd997d3f209b3dc09b266802b2d71
nguyenlamanh1701@gmail.com <nguyenlamanh1701@gmail.com>
2022-02-09 23:36:28 +0700

    check current user(token), cap nhat len fe

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,13 +4,39 @@ import Register from './components/Register';
 import Main from './components/Main';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './components/NotFound';
-import { useReducer } from 'react';
+import { useCallback, useEffect, useReducer } from 'react';
 import AppReducer from './reducers/AppReducer';
 import AppContext from './components/AppContext';
+import axios from 'axios';
 
 function App() {
   const initialState = { user: null, posts: [] };
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  const checkCurrentUser = useCallback(async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const option = {
+        method: 'get',
+        url: '/api/v1/auth',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      const response = await axios(option);
+      if (response.data.data.user) {
+        const { userName } = response.data.data.user;
+        dispatch({ type: 'CURRENT_USER', payload: { userName } });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    checkCurrentUser();
+  }, [checkCurrentUser]);
 
   return (
     <Router>
