5a60dc27967d2e67ce13c1cc9d8ffc29094c98cc
anh nguyen <nguyenlamanh1701@gmail.com>
2022-02-12 23:28:33 +0700

    ghep BE Postlise, PostItem

--- front-end/src/components/PostItem.js
+++ front-end/src/components/PostItem.js
@@ -1,51 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function PostItem() {
+export default function PostItem({ post }) {
+  const [openEditForm, setOpenEditForm] = useState(false);
+  const [openDeleteConfirm, setOpenDeleteConfirm] = useState(false);
+  let date = new Date(post.createdAt);
   return (
     <div className="post-item">
-      <p className="post-content">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias tempore
-        reiciendis, a sequi, reprehenderit rem odit nam deleniti odio voluptas
-        nemo maxime, facilis saepe in? Esse magnam doloremque veritatis
-        laudantium.
-      </p>
+      <p className="post-content">{post.content}</p>
       <div className="post-footer">
         <div className="post-infors">
-          <span>by AnhNguyen</span>
-          <span>Data: 15/11/2020</span>
+          <span>{post.author.name}</span>
+          <span>
+            Date:
+            {`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}
+          </span>
         </div>
-        <div className="post-edit-delete">
-          <span>Edit</span>
-          <span>Delete</span>
-          <span className="delete-question">Are you sure?</span>
-          <span>Yes</span>
-          <span>No</span>
-        </div>
-      </div>
-      <div className="post-edit-form">
-        <form className="edit-form">
-          <textarea
-            type="text"
-            name="content"
-            id="content"
-            className="content"
-            placeholder="What's happenging?"
-          >
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
-            velit delectus ex tempora quasi est error perspiciatis, quaerat amet
-            maiores recusandae modi veritatis. Qui deserunt ipsa, ab explicabo
-            repudiandae unde?
-          </textarea>
-          <div className="btn-container">
-            <button className="btn" type="button">
-              Update
-            </button>
-            <button className="btn" type="button">
-              Cancel
-            </button>
+        {post.isEditable && (
+          <div className="post-edit-delete">
+            {openDeleteConfirm ? (
+              <>
+                <span className="delete-question">Are you sure?</span>
+                <span>Yes</span>
+                <span onClick={() => setOpenDeleteConfirm(false)}>No</span>
+              </>
+            ) : (
+              <>
+                <span onClick={() => setOpenEditForm(true)}>Edit</span>
+                <span onClick={() => setOpenDeleteConfirm(true)}>Delete</span>
+              </>
+            )}
           </div>
-        </form>
+        )}
       </div>
+
+      {openEditForm && (
+        <div className="post-edit-form">
+          <form className="edit-form">
+            <textarea
+              type="text"
+              name="content"
+              id="content"
+              className="content"
+              placeholder="What's happenging?"
+            >
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
+              velit delectus ex tempora quasi est error perspiciatis, quaerat
+              amet maiores recusandae modi veritatis. Qui deserunt ipsa, ab
+              explicabo repudiandae unde?
+            </textarea>
+            <div className="btn-container">
+              <button className="btn" type="button">
+                Update
+              </button>
+              <button
+                className="btn"
+                type="button"
+                onClick={() => setOpenEditForm(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- front-end/src/components/PostList.js
+++ front-end/src/components/PostList.js
@@ -1,9 +1,12 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import PostItem from './PostItem';
 import '../css/Post.css';
 import axios from 'axios';
+import AppContext from './AppContext';
 
 export default function PostList() {
+  const { state, dispatch } = useContext(AppContext);
+  const { posts, user } = state;
   const getAllPosts = useCallback(async () => {
     try {
       const option = {
@@ -12,20 +15,33 @@ export default function PostList() {
       };
       const response = await axios(option);
       const posts = response.data.data.posts;
-      console.log(posts);
+      dispatch({ type: 'GET_ALL_POSTS', payload: posts });
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     getAllPosts();
   }, [getAllPosts]);
 
+  const newPosts = posts.map((post) => {
+    if (user) {
+      return post.author.name === user.userName
+        ? { ...post, isEditable: true }
+        : post;
+    } else {
+      return { ...post, isEditable: false };
+    }
+  });
+  console.log(newPosts);
+
   return (
     <section>
       <div className="post-list">
-        <PostItem></PostItem>
+        {newPosts.map((post, index) => (
+          <PostItem post={post} key={post._id} />
+        ))}
       </div>
     </section>
   );
